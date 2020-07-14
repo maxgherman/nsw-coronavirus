030d3b2bd3e1d304973c0d1dc0bd53da02abf6d7
Max Gherman <german.maxim@gmail.com>
2020-07-14 17:35:31 +1000

    Updates

---
+++ precache-manifest.579169d97ee4b764a2b4a52c66684a2c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5187e5e18d111841c3d1cb242b0a47f3",
+    "url": "/nsw-corona-virus/index.html"
+  },
+  {
+    "revision": "8714b981b8bd3f3de1b3",
+    "url": "/nsw-corona-virus/static/css/2.7597f154.chunk.css"
+  },
+  {
+    "revision": "50c9f51e23d2c4c63c20",
+    "url": "/nsw-corona-virus/static/css/main.89c7c0ea.chunk.css"
+  },
+  {
+    "revision": "8714b981b8bd3f3de1b3",
+    "url": "/nsw-corona-virus/static/js/2.68f684b7.chunk.js"
+  },
+  {
+    "revision": "1fbc5163fc4198124faa18b29c1957e4",
+    "url": "/nsw-corona-virus/static/js/2.68f684b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69ee2a8ff08c559390af",
+    "url": "/nsw-corona-virus/static/js/3.4d7feac7.chunk.js"
+  },
+  {
+    "revision": "bc1777f3a1e640628e8f",
+    "url": "/nsw-corona-virus/static/js/4.09414aa8.chunk.js"
+  },
+  {
+    "revision": "a02b35ec3719cabd79ba",
+    "url": "/nsw-corona-virus/static/js/5.fdced50f.chunk.js"
+  },
+  {
+    "revision": "9731a704cf469da7cf36",
+    "url": "/nsw-corona-virus/static/js/6.c3b98d8a.chunk.js"
+  },
+  {
+    "revision": "50c9f51e23d2c4c63c20",
+    "url": "/nsw-corona-virus/static/js/main.3b3a7de0.chunk.js"
+  },
+  {
+    "revision": "17abbecbf1ed32f4ac42",
+    "url": "/nsw-corona-virus/static/js/runtime-main.f4ce363a.js"
+  }
+]);
