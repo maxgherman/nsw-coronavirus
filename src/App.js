6db62644a484e89a8a297301ddd66e9b9736003b
Max Gherman <german.maxim@gmail.com>
2020-09-10 22:21:44 +1000

    Load data progress

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
 import React from 'react';
 import { useRoutes } from 'react-router-dom';
-import { ThemeProvider } from '@material-ui/core';
+import { ThemeProvider, makeStyles } from '@material-ui/core';
+import LinearProgress from '@material-ui/core/LinearProgress';
 import GlobalStyles from 'src/components/GlobalStyles';
 import { DataProvider, DataConsumer } from 'src//components/data';
 import theme from 'src/theme';
 import routes from 'src/routes';
 
+const useStyles = makeStyles(() => ({
+  progressWrapper: {
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    width: '50%',
+    height: '100%'
+  },
+  progress: {
+    position: 'relative',
+    top: '45%',
+    height: '5px'
+  }
+}));
+
 const App = () => {
+  const classes = useStyles();
   const routing = useRoutes(routes, process.env.PUBLIC_URL);
+
+  const renderBody = (data) => (data.loaded ? routing
+    : (
+      <div className={classes.progressWrapper}>
+        <LinearProgress className={classes.progress} />
+      </div>
+    ));
 
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
       <DataProvider>
-        <DataConsumer>{(data) => (data.loaded ? routing : null)}</DataConsumer>
+        <DataConsumer>
+          {renderBody}
+        </DataConsumer>
       </DataProvider>
     </ThemeProvider>
   );
