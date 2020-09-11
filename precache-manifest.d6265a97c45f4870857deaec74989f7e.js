13c6b8c7c42f6510aa82ecea95f806adac95bad9
maxgherman <maxgherman@users.noreply.github.com>
2020-09-11 01:15:54 +0000

    deploy: b6736a7c171437110fbcfd6badb7f0580f53b8d8

---
+++ precache-manifest.d6265a97c45f4870857deaec74989f7e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "799efe67ac84ba4b28ef91f5562ec063",
+    "url": "/nsw-coronavirus/index.html"
+  },
+  {
+    "revision": "f111c2d322c4d6485e4d",
+    "url": "/nsw-coronavirus/static/css/2.1b78fa86.chunk.css"
+  },
+  {
+    "revision": "87d39d3a72aa63afdc3f",
+    "url": "/nsw-coronavirus/static/css/main.60381171.chunk.css"
+  },
+  {
+    "revision": "f111c2d322c4d6485e4d",
+    "url": "/nsw-coronavirus/static/js/2.f3e14a16.chunk.js"
+  },
+  {
+    "revision": "8cdfce51c2194d093e2045a0d4d5e1d1",
+    "url": "/nsw-coronavirus/static/js/2.f3e14a16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87d39d3a72aa63afdc3f",
+    "url": "/nsw-coronavirus/static/js/main.2a983559.chunk.js"
+  },
+  {
+    "revision": "ff6cae082d4181a1ffe6",
+    "url": "/nsw-coronavirus/static/js/runtime-main.dbc0d4c3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nsw-corona-virus/precache-manifest.8b07788a81bdf8e9f44d846b131a74da.js"
+  "/nsw-coronavirus/precache-manifest.d6265a97c45f4870857deaec74989f7e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nsw-corona-virus/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nsw-coronavirus/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
