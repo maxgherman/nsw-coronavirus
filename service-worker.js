617333471e70b600a3493ea51fba6b87edaa3592
maxgherman <maxgherman@users.noreply.github.com>
2020-09-12 00:43:02 +0000

    deploy: 393a13967da2859c29f4e3457f9d2d26048b4f89

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nsw-coronavirus/precache-manifest.cbddacb49c2828b906b93c99c4e3d11b.js"
+  "/precache-manifest.b8264e7b22217a72b10186b012a9a040.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nsw-coronavirus/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
